faa92d25ddc74166a9e018f9692d346f40048e4e
Shreya Wani <shreyawani2006@gmail.com>
2026-04-18 23:59:01 +0530

    Add useAuth hook for managing authentication state and actions

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,37 @@
+import { useContext } from "react";
+import { AuthContext } from "../auth.content";
+import { login, register, logout, getMe } from "../services/auth.api.js"
+
+export const useAuth = () => {
+    const context = useContext(AuthContext)
+    const { user, setUser, loading, setLoading } = context
+
+    const handleLogin = async ({email, password}) => {
+        setLoading(true)
+        const data = await login({email, password})
+        setUser(data.user)
+        setLoading(false)
+    }
+
+    const handleRegister = async ({name, email, password}) => {
+        setLoading(true)
+        const data = await register({name, email, password})    
+        setUser(data.user)
+        setLoading(false)
+    }
+
+    const handleLogout = async () => {
+        setLoading(true)
+        await logout()
+        setUser(null)
+        setLoading(false)
+    }
+
+    return {
+        user,
+        loading,
+        handleLogin,
+        handleRegister,
+        handleLogout
+    }
+}
